e16239d0f9184f23f993b36895a3f3614c90d9ca
colin <zhang_colin@163.com>
2023-06-30 15:44:26 +0800

    feat: screen

---
+++ src/views/screen/components/tourist/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="box">
+    <div class="top">
+      <p class="title">实时游客统计</p>
+      <p class="bg"></p>
+      <p class="right">
+        可预约总量
+        <span>9999</span>
+        人
+      </p>
+    </div>
+    <div class="number">
+      <span v-for="(item, index) in people" :key="index">{{ item }}</span>
+    </div>
+    <div class="charts" ref="charts"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import * as echarts from 'echarts';
+import 'echarts-liquidfill';
+
+let people = ref('215908人');
+
+let charts = ref();
+
+onMounted(() => {
+  let myCharts = echarts.init(charts.value);
+  myCharts.setOption({
+    series: {
+      type: 'liquidFill',
+      data: [0.6, 0.5, 0.4],
+      radius: '90%',
+    },
+    grid: {
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    },
+  });
+});
+</script>
+
+<style scoped lang="scss">
+.box {
+  background: url(../../images/dataScreen-main-lb.png) no-repeat;
+  background-size: 100% 100%;
+  margin-top: 10px;
+
+  .top {
+    margin-left: 20px;
+    .title {
+      color: white;
+      font-size: 20px;
+    }
+
+    .bg {
+      width: 68px;
+      height: 7px;
+      background: url(../../images/dataScreen-title.png) no-repeat;
+      margin-top: 10px;
+    }
+
+    .right {
+      text-align: right;
+      color: white;
+      font-size: 20px;
+
+      span {
+        color: yellowgreen;
+      }
+    }
+  }
+
+  .number {
+    margin-top: 20px;
+    padding: 10px;
+    display: flex;
+
+    span {
+      flex: 1;
+      height: 40px;
+      text-align: center;
+      line-height: 40px;
+      background: url(../../images/total.png) no-repeat;
+      background-size: 100% 100%;
+      color: #29fcff;
+    }
+  }
+
+  .charts {
+    width: 100%;
+    height: 250px;
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,94 @@
 <template>
-  <h1>Screen</h1>
+  <div class="container">
+    <div class="screen" ref="screen">
+      <div class="top">
+        <Top />
+      </div>
+      <div class="bottom">
+        <div class="left">
+          <Tourist class="tourist" />
+          <Sex class="sex" />
+          <Age class="age" />
+        </div>
+        <div class="center">center</div>
+        <div class="right">right</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Top from './components/top/index.vue';
+import Tourist from './components/tourist/index.vue';
+import Sex from './components/sex/index.vue';
+import Age from './components/age/index.vue';
 
-<style></style>
+const screen = ref();
+
+onMounted(() => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`;
+});
+
+const getScale = (w = 1920, h = 1080) => {
+  const ww = window.innerWidth / w;
+  const wh = window.innerHeight / h;
+  return ww < wh ? ww : wh;
+};
+
+window.onresize = () => {
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%, -50%)`;
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100vw;
+  height: 100vh;
+  background: url(./images/bg.png) no-repeat;
+  background-size: cover;
+
+  .screen {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    width: 1920px;
+    height: 1080px;
+    transform-origin: left top;
+
+    .top {
+      widows: 100%;
+      height: 40px;
+    }
+
+    .bottom {
+      display: flex;
+
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+
+        .tourist {
+          flex: 1;
+        }
+
+        .sex {
+          flex: 1;
+        }
+
+        .age {
+          flex: 1;
+        }
+      }
+      .center {
+        flex: 2;
+      }
+      .right {
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
